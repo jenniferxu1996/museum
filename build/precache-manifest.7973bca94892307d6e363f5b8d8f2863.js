c45b95573a7f25b5510c968a071dfade7a162add
jenniferxu1996 <jenniferxu1996@outlook.com>
2018-11-26 23:26:02 +0000

    train

---
+++ build/precache-manifest.7973bca94892307d6e363f5b8d8f2863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cc49e2545201001e898",
+    "url": "./static/css/main.24a169d8.chunk.css"
+  },
+  {
+    "revision": "0cc49e2545201001e898",
+    "url": "./static/js/main.0cc49e25.chunk.js"
+  },
+  {
+    "revision": "e205d0b8a848381acab9",
+    "url": "./static/js/1.e205d0b8.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "dc0ae38dbfd266bebffa8bf78186000f",
+    "url": "./static/media/img_45.dc0ae38d.jpg"
+  },
+  {
+    "revision": "b8b881507633ca7561917fa5a1b07f28",
+    "url": "./index.html"
+  }
+];
